90748196f553fb2599d5dbef9b38ec6bed9909f7
aringler-usgs <aringler@usgs.gov>
2014-10-22 08:39:10 -0600

    Added wildcarding and saving location

--- dataconvert/switch2steim.py
+++ dataconvert/switch2steim.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import sys
-from obspy.core import read
+from obspy.core import read, Stream
 import numpy
+import glob
+import os
 
 
 #Get command line arguments
@@ -11,16 +13,29 @@ if len(sys.argv) < 3:
 	sys.exit(0)
 
 
-mseedfiles = sys.argv[1:][0]
-scalefactor = float(sys.argv[2:][0])
+
+mseedfiles = sys.argv[1:-1]
 
 
-st = read(mseedfiles)
 
 
-for tr in st:
-	tr.data = tr.data*scalefactor
-	tr.data=tr.data.astype(numpy.int32)
-	tr.stats.mseed['encoding'] = 4
 
-st.write(mseedfiles + "SCALED",format="MSEED")
+scalefactor = float(sys.argv[-1])
+
+
+
+
+for mseedfile in mseedfiles:
+	st = Stream()
+	st += read(mseedfile)
+
+
+	for tr in st:
+		tr.data = tr.data*scalefactor
+		tr.data=tr.data.astype(numpy.int32)
+		tr.stats.mseed['encoding'] = 4
+
+
+	curdir = os.getcwd()
+	mseedfileabb = mseedfile.split("/")[-1]
+	st.write(mseedfileabb + "SCALED",format="MSEED")
